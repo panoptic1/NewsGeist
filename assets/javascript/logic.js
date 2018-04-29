400107a110fd9558ca287121487f13d84e02f389
Nikolas  Haug <nikolashaug@Nikolass-MacBook-Air.local>
2018-04-29 13:39:29 -0500

    add headlines

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
-
-    var sources = [
-        "the-new-york-times",
-        "al-jazeera-english",
-        "reuters"
-    ]
-    
-    var results = [];
-    
-    var newQueryURL = [];
     
     //create a variable for the news sources array
     var newsSources = [];
 
+    //Variables for categories and the top headlines api url
+    var category = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+
+    function shuffleArray() {
+        for(var i = category.length -1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var test = category[i];
+            category[i] = category[j];
+            category[j] = test;
+            console.log(test);
+        }
+    }
 
     $('#search-button').click(function(event) {
       event.preventDefault();
 
     //   function dumpNews() {
 
+        //get the keyword term from the input search bar
         var term = $('#term').val().trim();
+            //clear the called articles if exist
             $('#headlinesContainer').empty();
             //Variables for keyword and API url
             var queryURL = "https://newsapi.org/v2/everything?q=" + term + "&pageSize=50&sources=al-jazeera-english,bbc-news,cnn,fortune,fox-news,msnbc,rt,the-economist,the-new-york-times,the-wall-street-journal,the-washington-post,vice-news,time,the-huffington-post,reuters,reddit-r-all,buzzfeed" + "&apiKey=8f648fabfb73464184ecb3df91ad60f5"
@@ -47,18 +51,20 @@ $(document).ready(function() {
                     var articleDescription = response.articles[i].description;
                     var articleURL = response.articles[i].url;
                     var articleDate = response.articles[i].publishedAt;
-    
                     // make DOM variable containers
                     var newsDiv = $('<div class="m-2">');
-                    
                     //create variable to write to html
                     var source = $('<h6>').text(articleSource);
+                    var URLtag = $('<a>').attr({
+                        "href": articleURL,
+                        "target": "_blank"
+                    });
                     var title = $('<h5>').text(articleTitle);
+                    URLtag.append(title);
                     var summary = $('<p>').text(articleDescription);
-    
                     //append to the DOM
                     newsDiv.append(source);
-                    newsDiv.append(title);
+                    newsDiv.append(URLtag);
                     newsDiv.append(summary);
                     $('#headlinesContainer').append(newsDiv);
                 }
@@ -68,28 +74,49 @@ $(document).ready(function() {
       
     });
  
+    //Create a function to grab 3 random top head\
+    function topHeadlines(){
+        for(var i = 0; i < 3; i++){
+        
+        //Variables for categories and the top headlines api url
+        var category = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
 
-
-
-
-
-function callNewsAPI(URL) {
-    for(var j = 0; j < URL.length; j++) {
+        var queryURL = "https://newsapi.org/v2/top-headlines?category=" + category[i] + "&pageSize=1&apiKey=8f648fabfb73464184ecb3df91ad60f5"
+        console.log(queryURL);
         $.ajax({
-            url: URL[j],
+            url: queryURL,
             method: "GET"
-        }).then(function(response){
-            // console.log(response);
-            results.push(response);
-            console.log(JSON.parse(JSON.stringify(results)));
-            // console.log(results[0].articles[0].source);
-        })
+        }).then(function(response) {
+                for(var i = 0; i < response.articles.length; i++) {
+                    //make headline variables
+                    var headlineSource = response.articles[i].source.name;
+                    var headlineTitle = response.articles[i].title;
+                    var headlineImage = response.articles[i].urlToImage;
+                    console.log(headlineImage);
+                    var headlineURL = response.articles[i].url;
+                    // make DOM variable containers
+                    var headlinesDiv = $('<div class="col-4">');
+                    //create variable to write to html
+                    var source = $('<h6>').text(headlineSource);
+                    var URLtag = $('<a>').attr({
+                        "href": headlineURL,
+                        "target": "_blank"
+                    });
+                    var title = $('<h5>').text(headlineTitle);
+                    var image = $('<img class="img-fluid">').attr("src", headlineImage);
+                    URLtag.append(title);
+                    URLtag.append(image);
+                    //append to the DOM
+                    headlinesDiv.append(source);
+                    headlinesDiv.append(URLtag);
+                    $('#top-headlines').append(headlinesDiv);
+                }
+        });
     }
-    // console.log(results);
-    console.log(JSON.parse(JSON.stringify(results)));
 }
 
-
+topHeadlines();
+shuffleArray();
 
 // dumpNews();
 
